cbb709dbe111a551bc7f50616944518a01407a0e
Luis Garcia <luisgarcia26284@gmail.com>
2022-05-22 21:35:14 +0800

    Adding opencl. Kernels and more coming

--- cmake/Modules/FindOpenCL.cmake
+++ cmake/Modules/FindOpenCL.cmake
@@ -1,6 +1,12 @@
 # For now we focus on mac/unix-like
 # We also search for OpenCL in the NVIDIA SDK default location
-FIND_PATH(OPENCL_INCLUDE_DIR CL/cl.h /opt/AMDAPPSDK-2.9-1/include/ )
+set(CUSTOM_DIR  $ENV{HOME}/Projects/Libraries/OpenCL/include/)
+set(CUSTOM_DIR_LB $ENV{HOME}/Projects/Libraries/OpenCL/lib/)
+message("My HOME VARIABLE IS : ${CUSTOM_DIR}")
+FIND_PATH(OPENCL_INCLUDE_DIR CL/opencl.hpp
+    HINTS ${CUSTOM_DIR}
+    )
+message("OPENCL_INCLUDE_DIR: ${OPENCL_INCLUDE_DIR}")
 FIND_LIBRARY(OPENCL_LIBRARIES OpenCL 
     ENV LD_LIBRARY_PATH
 )
--- playground/opencl_kernels.cpp
+++ playground/opencl_kernels.cpp
@@ -1,10 +1,18 @@
+// #defin CL_TARGET_OPENCL_VERSION 120
+#include <CL/opencl.hpp>
 #include <iostream>
 #include <vector>
 #define SIZE 100
 
+std::string program_string{
+R"(
+
+)"
+};
+
 int main(){
 
-    std::vector<float> h_a(SIZE), h_b(SIZE), h_c(SIZE);
+    std::vector<double> h_a(SIZE), h_b(SIZE), h_c(SIZE);
 
     //Populate a and b 
     for(size_t i = 0;i<SIZE;i++){
@@ -12,6 +20,24 @@ int main(){
         h_b[i] = 200;
     }
 
+    std::cout << "Now Setting up the OPENCL Compute Environment" <<std::endl; 
 
+    //Creating a Context
+    // cl::Context context(CL_DEVICE_TYPE_DEFAULT);
+    const cl::Context context(CL_DEVICE_TYPE_DEFAULT);
+
+    //Create Command QUeue
+    cl::CommandQueue queue(context);
+
+    cl::Program program(context,program_string,true);
+    //Set up Memory  Objects
+    
+    using iterator_type = std::vector<double>::iterator;
+    //I belive these ones fetch their own context
+    cl::Buffer d_a(h_a.begin(), h_a.end(),true);
+    cl::Buffer d_b(h_b.begin(), h_b.end(),true);
+    cl::Buffer d_c(context, CL_MEM_READ_WRITE,sizeof(double)*SIZE);
+
+    // cl::copy();
 
 }
